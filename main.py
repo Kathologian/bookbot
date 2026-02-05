15a0e7ce275dd29ff27e5cef9b1ca4761539d52d
Kathologian <steve.beck799@web.de>
2026-02-05 18:15:10 +0100

    Added data analysis functionality (counting letters and returning their values

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from stats import get_book_text
 from stats import counting_distinct_characters
 
 def main():
-    #print('============ BOOKBOT ============')
-    #print('Analyzing book found at books/frankenstein.txt...')
-    #print('----------- Word Count ----------')
+    print('============ BOOKBOT ============')
+    print('Analyzing book found at books/frankenstein.txt...')
+    print('----------- Word Count ----------')
     print(f'Found {get_book_text('./books/frankenstein.txt')} total words')
-    #print('--------- Character Count -------')
+    print('--------- Character Count -------')
     print(f'{counting_distinct_characters('./books/frankenstein.txt')}')
     #print('============= END ===============')
 
