d4d1bbfba49bf24f01e13e54d78a3555626460db
Kathologian <steve.beck799@web.de>
2026-02-05 13:15:31 +0100

    Added data analysis functionality

--- main.py
+++ main.py
@@ -1,63 +1,14 @@
-from stats import counting_words_in_book
+from stats import get_book_text
 from stats import counting_distinct_characters
 
-"""def get_book_text(filepath):
-  with open(filepath) as f:
-    contents = f.read()
-    return contents"""
-
-
 def main():
-  print(f"Found {counting_words_in_book("./books/frankenstein.txt")} total words")
-  print(f"{counting_distinct_characters("./books/frankenstein.txt")}")
-  #print(f"{get_book_text("./books/frankenstein.txt")}")
+    #print('============ BOOKBOT ============')
+    #print('Analyzing book found at books/frankenstein.txt...')
+    #print('----------- Word Count ----------')
+    print(f'Found {get_book_text('./books/frankenstein.txt')} total words')
+    #print('--------- Character Count -------')
+    print(f'{counting_distinct_characters('./books/frankenstein.txt')}')
+    #print('============= END ===============')
 
-if __name__== "__main__":
-  main()
-
-
-"""
-def number_of_words(contents): # this should take the result of def get_book_text and count the words to return the word count
-  word_count = 0
-  contents = contents.get_book_text()
-  contents.split()
-  for word in contents:
-    word_count += 1
-  return word_count
-
-
-def main(): #The main function that calls on the get_book_text and number_of_word functions
-  word_count = word_count.number_of_words
-  filepath = "./books/frankenstein.txt"
-  get_book_text(filepath)
-  print(f"{word_count} words found in the document")
-
-
-main()
-
-# This works 
-
-def get_book_text(filepath):
-  with open(filepath) as f:
-    contents = f.read()
-  print(contents)
-
-def main():
-  filepath = "./books/frankenstein.txt"
-  get_book_text(filepath)
-
-main()
------------------------------------------
-def get_book_text(filepath):
-  with open(filepath) as f:
-    contents = f.read()
-  print(contents)
-
-def main():
-  print(f"{get_book_text("./books/frankenstein.txt")}")
-
-if __name__=="__main__":
-  main()
-  
-"""
-  
+if __name__ == '__main__':
+    main()
